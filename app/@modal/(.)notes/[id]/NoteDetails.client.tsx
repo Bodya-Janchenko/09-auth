fc9ad02e66bc3c64d5118f3145afbf4d8562efce
Bogdan <robotosoid2000@gmail.com>
2025-11-07 16:53:31 +0200

    fix bug

--- app/@modal/(.)notes/[id]/NoteDetails.client.tsx
+++ app/@modal/(.)notes/[id]/NoteDetails.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
-import { fetchNoteById } from "@/lib/api/api";
+import { fetchNoteById } from "@/lib/api/clientApi";
 import { useRouter } from "next/router";
 import { Note } from "@/types/note";
 import Modal from "@/components/Modal/Modal";
